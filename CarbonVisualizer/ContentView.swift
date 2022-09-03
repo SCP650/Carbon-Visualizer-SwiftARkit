19c388bdc9d1bb95a71bd3eec0bd2918e0b0ff8a
Sebastian Yang <yukaiy@andrew.cmu.edu>
2022-09-03 11:35:04 -0700

    added focus entity

--- CarbonVisualizer/ContentView.swift
+++ CarbonVisualizer/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 import RealityKit
 import ARKit
+import FocusEntity
 
 struct ContentView : View {
     @State private var placeObject = false
     var body: some View {
-        ZStack{ ARViewContainer(triggerObject:self.$placeObject).edgesIgnoringSafeArea(.all)
-            Button("Visualize"){
+        VStack{ ARViewContainer(triggerObject:self.$placeObject).edgesIgnoringSafeArea(.all)
+            Button(placeObject ? "Reset":"Visualize"){
                 placeObject = !placeObject
             }
         }
@@ -23,27 +24,22 @@ struct ContentView : View {
 struct ARViewContainer: UIViewRepresentable {
     @Binding var triggerObject: Bool
     
-    func makeUIView(context: Context) -> ARView {
-        let arView = ARView(frame: .zero)
-        let config = ARWorldTrackingConfiguration()
-        config.planeDetection = [.horizontal]
-        config.environmentTexturing = .automatic
-        if(ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh)){
-            config.sceneReconstruction = .mesh
-        }
-        arView.session.run(config)
+    func makeUIView(context: Context) -> CustomARView {
+        let arView = CustomARView(frame: .zero)//ARView(frame: .zero)
         return arView
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {
+    func updateUIView(_ uiView: CustomARView, context: Context) {
         if(self.triggerObject){
             let boxModelEntity = createBoxModelEntity()
             let anchorEntity = AnchorEntity(plane: .horizontal)
             anchorEntity.addChild(boxModelEntity)
             uiView.scene.addAnchor(anchorEntity)
+            uiView.HideFocusEntity()
         }else{
             uiView.scene.anchors.removeAll()
+            uiView.showFocusEntity()
         }
     }
     
@@ -53,6 +49,37 @@ struct ARViewContainer: UIViewRepresentable {
         return ModelEntity(mesh: boxResource, materials: [myMaterial])
     }
     
+}
+
+class CustomARView:ARView{
+    var focusEntity: FocusEntity?
+    required init(frame frameRect: CGRect) {
+        super.init(frame: frameRect)
+        self.setupARVIew()
+        self.focusEntity = FocusEntity(on: self, style: .classic(color: .yellow))
+    }
+    
+    func setupARVIew(){
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal]
+        config.environmentTexturing = .automatic
+        if(ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh)){
+            config.sceneReconstruction = .mesh
+        }
+        self.session.run(config)
+    }
+    
+    public func showFocusEntity(){
+        self.focusEntity = FocusEntity(on: self, style: .classic(color: .yellow))
+    }
+    
+    public func HideFocusEntity(){
+        self.focusEntity?.destroy()
+    }
+    
+    @objc required dynamic init?(coder decoder: NSCoder) {
+       fatalError("init(coder:) has not been implemented")
+     }
 }
 
 #if DEBUG
