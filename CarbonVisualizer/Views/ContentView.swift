2416900d07e5961fd7ad7183d1122e0dda4cc12b
Sebastian Yang <yukaiy@andrew.cmu.edu>
2022-09-03 21:27:03 -0700

    more refactor

--- CarbonVisualizer/Views/ContentView.swift
+++ CarbonVisualizer/Views/ContentView.swift
@@ -9,18 +9,9 @@ import SwiftUI
 
 
 struct ContentView : View {
-    @State private var placeObject = false
-    @State private var kgOfCo2 : Float = 24.48
+
     var body: some View {
-        VStack{ ARViewContainer(triggerObject:self.$placeObject, kgOfCo2: self.$kgOfCo2).edgesIgnoringSafeArea(.all)
-            HStack{
-                Button(placeObject ? "Reset":"Visualize"){
-                    placeObject = !placeObject
-                }
-                TextField("Kg of Co2", value: self.$kgOfCo2, formatter: NumberFormatter())
-            }
-       
-        }
+        VisualizerView()
     }
 }
 
---
+++ CarbonVisualizer/Views/VisualizerView.swift
@@ -0,0 +1,24 @@
+//
+//  VisualizerView.swift
+//  CarbonVisualizer
+//
+//  Created by Atlas on 9/3/22.
+//
+
+import SwiftUI
+
+struct VisualizerView: View {
+    @State private var placeObject = false
+    @State private var kgOfCo2 : Float = 24.48
+    var body: some View {
+        VStack{ ARViewContainer(triggerObject:self.$placeObject, kgOfCo2: self.$kgOfCo2).edgesIgnoringSafeArea(.all)
+            HStack{
+                Button(placeObject ? "Reset":"Visualize"){
+                    placeObject = !placeObject
+                }
+                TextField("Kg of Co2", value: self.$kgOfCo2, formatter: NumberFormatter())
+            }
+        }
+    }
+}
+
