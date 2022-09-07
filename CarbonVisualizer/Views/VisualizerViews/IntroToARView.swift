1312894d4d108eb7356ba3a2ba3f2c704edcf088
Sebastian Yang <yukaiy@andrew.cmu.edu>
2022-09-06 20:30:49 -0700

    improve intro to ar view

--- CarbonVisualizer/Views/VisualizerViews/IntroToARView.swift
+++ CarbonVisualizer/Views/VisualizerViews/IntroToARView.swift
@@ -9,15 +9,39 @@ import SwiftUI
 
 struct IntroToARView: View {
     @ObservedObject var viewModel : CalculatorViewModel
-    
+    @State var showInfo : Bool = false
+    func hideKeyboard() {
+          UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+      }
     var body: some View {
         VStack{
-            Text(viewModel.resultDailyKg)
+            Spacer()
+            Text("Your Daily Emission is...").font(.title)
+            HStack{
+            TextField("",
+                      text: $viewModel.resultDailyKg
+                  )
+                  .textFieldStyle(.roundedBorder)
+                  .keyboardType(.decimalPad)
+                .frame(width: 200, height: 30, alignment: .center)
+                Text("kg")
+                Button(action: { showInfo = true}) {
+                    Image(systemName: "questionmark.circle")
+                }
+                  .popover(isPresented: $showInfo) {
+                      Text("Explan Daily Result")
+                          .padding()
+                  }
+            }
+            Spacer()
             Button( action: {}, label: {
-                NavigationLink(destination: VisualizerView(kgOfCo2: Float(viewModel.resultDailyKg)!)) {
+                NavigationLink(destination: VisualizerView(kgOfCo2: Float(viewModel.resultDailyKg) ?? 0)) {
                 Text("Call to action")
                 }
         })
+            Spacer()
+        }.onTapGesture {
+            hideKeyboard()
         }
         
         
